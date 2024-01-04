aca1a2c2eb6cb307c317c48dd90741fabcbd3b04
hassan848 <72468804+hassan848@users.noreply.github.com>
2024-01-04 22:54:23 +0000

    config files for main python scripts

---
+++ config/playlists.py
@@ -0,0 +1,5 @@
+def pop_playlists():
+    playlist_dict = {'top 100 pop': 'spotify:playlist:31ymdYCITDnZRtkKzP3Itp', 
+                     'pop hits 2000s':'spotify:playlist:6mtYuOxzl58vSGnEDtZ9uB', 
+                     'pop mix': 'spotify:playlist:37i9dQZF1EQncLwOalG3K7'}
+    return playlist_dict
